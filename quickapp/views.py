f8dcc854395d7e78b51ec5f7b45b9c064d7a4826
kev521-design <josephmenywa32@gmail.com>
2025-02-18 16:19:32 +0300

    initial commit

---
+++ quickapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+
+def about(request):
+    return render(request,'about.html')
